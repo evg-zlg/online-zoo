b25b4bfd80e6545e71a94c3394f6d437cfcf9248
evg-zlg <evg.zalogin@gmail.com>
2022-10-09 19:28:44 +0500

    feat: amount radio and input work

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,12 +1,36 @@
 const burgerIcon = document.querySelector(".menu__burger-icon");
+const menu = burgerIcon.parentElement.childNodes[3];
+const body = document.querySelector("body");
+
 burgerIcon.addEventListener("click", (e) => {
-  burgerIcon.parentElement.childNodes[3].classList.add("menu_burger-open");
-  document.querySelector(".header").childNodes[1].childNodes[1].childNodes[1].classList.add("logo_orange");
-  document.querySelector(".menu_burger-open").addEventListener("click", (e) => {
+
+  //function closed burger menu
+  function closeBurgerMenu() {
     if (document.querySelector(".menu_burger-open")) {
       document.querySelector(".header").childNodes[1].childNodes[1].childNodes[1].classList.remove("logo_orange");
-    document.querySelector(".menu_burger-open").classList.remove("menu_burger-open");
+      document.querySelector(".menu_burger-open").classList.remove("menu_burger-open");
+    } 
+  };
+  //open beurger menu
+  burgerIcon.parentElement.childNodes[3].classList.add("menu_burger-open");
+  //change color of logo
+  document.querySelector(".header").childNodes[1].childNodes[1].childNodes[1].classList.add("logo_orange");
+  // block scroll for body 
+  body.classList.add("body_popup_open");
+  
+  body.addEventListener("click", (e) => {
+    // if click out of menu - close burger menu
+    if ( ( ! e.path.includes(menu)) && (e.target !== burgerIcon) ) {
+      closeBurgerMenu();
+      body.classList.remove("body_popup_open");
+      body.removeEventListener;
     }
   });
+
+  //close burger menu after click to close-icon
+  document.querySelector(".close-icon").addEventListener("click", (e) => {
+      closeBurgerMenu();
+  });
+  
 });
 
