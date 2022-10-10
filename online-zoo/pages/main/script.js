21aa53c81ded84951a5080d2d10bf87b69961363
evg-zlg <evg.zalogin@gmail.com>
2022-10-10 15:18:22 +0500

    feat: shuffle animal cards on load page

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -34,3 +34,54 @@ burgerIcon.addEventListener("click", (e) => {
   
 });
 
+//take all cards with animal
+const animalCards = document.querySelectorAll(".cards__card");
+
+//function shuffle array for nodelist
+function shuffleArray(arr) {
+  const newArr = Array.from(arr);
+  let j = 0;
+  let temp = 0;
+  for (let i = newArr.length - 1; i > 0 ; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = newArr[i];
+    newArr[i] = newArr[j];
+    newArr[j] = temp;
+  };
+  return newArr;
+}
+
+//function create div with new order cards
+function createNewOrderCards(arr) {
+  let div = document.createElement("div");
+  div.className = "cards__items";
+  arr.forEach(card => {
+    div.append(card);
+  });
+  return div;
+}
+
+//repair hidden modificator
+function repairHidden(arr, leng) {
+  for (let i = 0; i < arr.childNodes.length; i++) {
+    if (i < leng) {
+      if (arr.childNodes[i].classList.contains("cards__card_hidden")) {
+        arr.childNodes[i].classList.remove("cards__card_hidden");
+      }
+    } else {
+      if ( ! arr.childNodes[i].classList.contains("cards__card_hidden")) {
+        arr.childNodes[i].classList.add("cards__card_hidden")
+      }
+    }
+  }
+}
+
+//random cards after load page
+document.addEventListener("DOMContentLoaded", () => {
+  const countAnimals = animalCards.length;
+  let oldCards = document.querySelector(".cards__items");
+  let newOrder = shuffleArray(animalCards);
+  let newCards = createNewOrderCards(newOrder);
+  repairHidden(newCards, 6);
+  document.querySelector(".cards").childNodes[1].replaceChild(newCards, oldCards);
+});
